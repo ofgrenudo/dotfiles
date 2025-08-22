5bce398e1a4542edf8cabce089e5063210f2af48
Joshua Winters-Brown <jwintersbro@Joshuas-MacBook-Air.local>
2025-08-22 19:59:26 -0400

    fix: theme changes

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  -- Add gruvbox plugin
+  { "ellisonleao/gruvbox.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
