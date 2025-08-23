c6d11644f8cca7c8ec863901e48e86888f2ff8f5
Joshua Winters-Brown <jwintersbro@Joshuas-MacBook-Air.local>
2025-08-22 20:00:20 -0400

    feat: install script

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 cp -fv ./bashrc ~/.bashrc
 cp -fv ./zshrc ~/.zshrc
 
+# append .rc files to functional files
+cat *.rc >>~/.zshrc
+cat *.rc >>~/.bashrc
+
 mkdir -p ~/.config/nvim
 cp -frv ./nvim/ ~/.config/nvim
