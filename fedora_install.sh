498aa666de785f8e31924e9642af0ed1606dd0a3
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2025-10-17 10:50:41 -0400

    chore: fedora inst scritp

---
+++ fedora_install.sh
@@ -0,0 +1,7 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo dnf install gcc clang make golang zsh rust cargo python3-pip nvim ripgrep fd-find pnpm npm nodejs nvm
+
+# Install lazygit
+sudo dnf copr enable dejan/lazygit
+sudo dnf install lazygit
+
