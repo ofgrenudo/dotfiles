6b2a6847be60ce84b40cd6f86da9489343dd6620
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2025-10-27 11:02:33 -0400

    chore: attempting to be declarative. idk.

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,7 +1,6 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo dnf install gcc clang make golang zsh rust cargo python3-pip nvim ripgrep fd-find pnpm npm nodejs nvm
+sudo dnf install gcc clang make golang zsh rust cargo python3-pip nvim ripgrep fd-find pnpm npm nodejs filezilla remmina fooyin
 
 # Install lazygit
 sudo dnf copr enable dejan/lazygit
 sudo dnf install lazygit
-
